501a05b1f02bf7e3edbe04045ebe8dee70aa9fcb
MCAlmond <31973758+MCAlmond@users.noreply.github.com>
2017-10-17 06:09:25 -0500

    Tarea de leap year

---
+++ src/pa/edu/uip/Mientras.java
@@ -0,0 +1,24 @@
+package pa.edu.uip;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int edad;
+        BufferedReader xx = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.print("Edad: ");
+            edad = Integer.parseInt(xx.readLine());
+            if (edad > 0 && edad <= 100) {
+                break;
+            } else {
+                System.out.println("Invalida. Vuelve a ingresar la edad.");
+            }
+        }
+        System.out.println(edad);
+    }
+}
